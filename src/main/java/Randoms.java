aa118181b6e16e11590a72af65c889ce0e90cc9d
Petroivch <petr.tikh03@gmail.com>
2022-07-07 12:41:52 +0400

    with bounded nextInt

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,5 +1,6 @@
 import java.util.Iterator;
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class Randoms implements Iterable<Integer> {
 
@@ -23,8 +24,31 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                return random.nextInt(min, max);
+                return boundedNextInt(min, max);
             }
         };
+    }
+
+    public static int boundedNextInt(int origin, int bound) {
+        Random rnm = new Random();
+        int r = rnm.nextInt();
+        if (origin < bound) {
+            final int n = bound - origin;
+            final int m = n - 1;
+            if ((n & m) == 0) {
+                r = (r & m) + origin;
+            } else if (n > 0) {
+                for (int u = r >>> 1;
+                     u + m - (r = u % n) < 0;
+                     u = rnm.nextInt() >>> 1)
+                    ;
+                r += origin;
+            } else {
+                while (r < origin || r >= bound) {
+                    r = rnm.nextInt();
+                }
+            }
+        }
+        return r;
     }
 }
